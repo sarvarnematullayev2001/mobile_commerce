c669c6f4be0e69fc9afc3f53679a7e7a09ff8a92
sarvar <tolibbeknur2@gmail.com>
2022-06-04 18:25:56 +0500

    db and front side of the project is almost done

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mobile_ecommerce',
+        'USER': 'sarvar',
+        'PASSWORD': 'sarvarjon12102001',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -121,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static'
+STATICFILES_DIRS = [
+    BASE_DIR + '/config/static',
+]
