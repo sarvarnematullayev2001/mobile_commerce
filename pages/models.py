2dca2d32a4dbd5059cd77f01914763c7df904772
sarvar <tolibbeknur2@gmail.com>
2022-06-04 19:46:12 +0500

    Teams models added

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Teams(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_link = models.URLField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Teams'
+    
+    def __str__(self):
+        return self.first_name 
+     
+    
